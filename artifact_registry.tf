e188f2d8c36932ae4756c5d9f75c02a8dafb0e5f
Viktor Lidholt <viktorlidholt@gmail.com>
2023-03-13 17:00:13 +0100

    Removes creation of the Artifact Registry

--- artifact_registry.tf
+++ artifact_registry.tf
@@ -1,7 +1,7 @@
 # Artifact registry, where we store your Docker images.
 
-resource "google_artifact_registry_repository" "containers" {
-  location      = var.region
-  repository_id = "serverpod-${var.runmode}-container"
-  format        = "DOCKER"
-}
+# resource "google_artifact_registry_repository" "containers" {
+#   location      = var.region
+#   repository_id = "serverpod-${var.runmode}-container"
+#   format        = "DOCKER"
+# }
--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,3 @@ output "network" {
   description = "The name of Serverpod's private network."
   value       = google_compute_network.serverpod.name
 }
-
-output "artifact_registry_id" {
-  description = "The ID of Serverpod's Artifact Registry."
-  value       = google_artifact_registry_repository.containers.id
-}
