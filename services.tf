181aee8f1986c3e125efe08065f8dcd3d9769c4a
Viktor Lidholt <viktorlidholt@gmail.com>
2023-03-23 11:35:44 +0100

    Enable services

---
+++ services.tf
@@ -0,0 +1,18 @@
+locals {
+  gcp_service_list = [
+    "artifactregistry.googleapis.com",
+    "certificatemanager.googleapis.com",
+    "dns.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "compute.googleapis.com",
+    "servicenetworking.googleapis.com",
+    "sqladmin.googleapis.com"
+  ]
+}
+
+resource "google_project_service" "gcp_services" {
+  count              = length(local.gcp_service_list)
+  project            = var.project
+  service            = local.gcp_service_list[count.index]
+  disable_on_destroy = false
+}
