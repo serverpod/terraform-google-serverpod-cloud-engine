e582104f508585ee68c00a4a6067e27533186c68
Viktor Lidholt <viktorlidholt@gmail.com>
2023-03-23 11:35:28 +0100

    Add more version details

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 # Main setup.
 
 terraform {
+  required_version = ">= 1.3.9"
+
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.51.0"
+      version = ">= 4.51.0"
     }
   }
 }
