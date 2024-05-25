a94f56b22733294847cbe85dc399971ed9f16b8a
Luke Hutchison <luke.hutch@gmail.com>
2024-05-25 14:11:27 -0600

    Support `redis_version`

--- redis.tf
+++ redis.tf
@@ -7,5 +7,7 @@ resource "google_redis_instance" "serverpod" {
   tier           = var.redis_tier
   memory_size_gb = var.redis_memory_size_gb
 
+  redis_version = var.redis_version
+
   authorized_network = google_compute_network.serverpod.id
 }
